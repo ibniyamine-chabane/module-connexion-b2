f5b9b37bea7bc2f95d43a7d499717567aaf8151f
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-09-11 10:25:32 +0200

    header responsive done

---
+++ header.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="navbar">
+        <div class="logo">
+            <a href="#">Module de connexion</a>
+        </div>
+        <ul class="links">
+            <li><a href="hero">Accueil</a></li>
+            <!-- <li><a href="hero">media</a></li>
+            <li><a href="hero">A Propos</a></li>
+            <li><a href="hero">Contact</a></li> -->
+        </ul>
+        <div class="buttons">
+            <a class="action-button pro">Inscription</a>
+            <a class="action-button">Connexion</a>
+        </div>
+        <div class="burger-menu-button">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+    </div>
+    <div class="burger-menu">
+        <ul class="links">
+            <li><a href="hero">Accueil</a></li>
+            <!-- <li><a href="hero">media</a></li>
+            <li><a href="hero">A Propos</a></li>
+            <li><a href="hero">Contact</a></li> -->
+            <div class="divider"></div>
+            <div class="buttons-burger-menu">
+                <a class="action-button pro">Insciption</a>
+                <a class="action-button">Connexion</a>
+            </div>
+        </ul>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,300&display=swap" rel="stylesheet">
+    <script defer src="js/script.js"></script>
+    <title>Navbar</title>
+</head>
+<body>
+    <?php require_once("header.php"); ?>
+    
+</body>
+</html>
