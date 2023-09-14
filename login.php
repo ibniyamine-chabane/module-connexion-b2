3c272e6accb5a27ff10fa4ae006a3892c9b045df
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-09-14 12:52:31 +0200

    all redirection acces made according to user logged or not

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php 
 session_start();
+
+if(isset($_SESSION['id_user'])) {
+    header("Location: index.php");
+}
+
 require_once("class/user.php");
 $moduleConnection = new User;
 $message = "";
